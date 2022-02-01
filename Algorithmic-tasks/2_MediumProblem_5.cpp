489d3df34b612c604366a47844a76933d9789f34
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-02-01 09:49:00 +0300

    The 8th task "String to Integer (atoi)" LeetCode

--- Algorithmic-tasks/2_MediumProblem_5.cpp
+++ Algorithmic-tasks/2_MediumProblem_5.cpp
@@ -1,4 +1,4 @@
-﻿//8. String to Integer(atoi)
+//8. String to Integer(atoi)
 //Implement the myAtoi(string s) function, which converts a string to a 32 - bit signed integer(similar to C / C++'s atoi function).
 //
 //The algorithm for myAtoi(string s) is as follows :
@@ -8,8 +8,8 @@
 //3. Read in next the characters until the next non - digit character or the end of the input is reached.The rest of the string is ignored.
 //4. Convert these digits into an integer(i.e. "123" -> 123, "0032" -> 32).If no digits were read, then the integer is 0. 
 //Change the sign as necessary(from step 2).
-//5. If the integer is out of the 32 - bit signed integer range[-231, 231 - 1], then clamp the integer so that it remains in the range.Specifically, 
-//integers less than - 231 should be clamped to - 231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+//5. If the integer is out of the 32 - bit signed integer range[-2^31, 2^31 - 1], then clamp the integer so that it remains in the range.Specifically, 
+//integers less than - 2^31 should be clamped to - 2^31, and integers greater than 2^31 - 1 should be clamped to 2^31 - 1.
 //5. Return the integer as the final result.
 //
 //Note:
@@ -31,35 +31,55 @@
 // Step 3 : "42" ("42" is read in)
 // ^
 // The parsed integer is 42.
-// Since 42 is in the range[-231, 231 - 1], the final result is 42.
+// Since 42 is in the range[-2^31, 2^31 - 1], the final result is 42.
 
-#include <iostream>
-#include <string>
+#include <iostream>       
+#include <string>          
+                         
 
-class Solution 
-{
-public:
-	int myAtoi(std::string s) 
+int myAtoi(std::string s)              //*****************в эту функцию можно передавать любое ДЕРЬМО, она всё равно не сломается******************
+{                                     //Эта задача была придумана человеком с очень интересной фантазией, только 16.4% решающих эту задачу, смогли её решить
+	bool positive = true;         //и у неё максимальное количество дизлайков)
+	double long result = 0;
+	int i = 0;
+
+	while (s[i] == ' ')
+		i++;
+
+	if (s[i] == '-' || s[i] == '+')
 	{
-		bool positive = true;
-		int result = 0;
-		for (int i = 0; i < s.length(); i++)
-		{
-			if (s[i] - '0' >= 0 && s[i] - '0' <= 9)
-			{
-				result += (int)s[i] - '0';
-				result *= 10;
-			}
-		}
-		return result/10;
+		positive = s[i] == '-' ? false : true;
+		i++;
 	}
-};
 
-int main()
-{
-	Solution s;
+	while (s[i] - '0' >= 0 && s[i] - '0' <= 9)
+	{
+		result += (double)s[i] - '0';
+		result *= 10;
+		i++;
+	}
 
-	std::cout << "RESULT: " << s.myAtoi("42") << std::endl;
+	result /= 10;
+
+	if (!positive)
+		result = -result;
+	if (result < std::pow(-2, 31))
+		return pow(-2, 31);
+	else if (result > std::pow(2, 31) - 1)
+		return pow(2, 31) - 1;
+
+	return result;
+}
+
+
+int main()                                                     //Examples input:
+{                                                              // "   +0 123"
+	std::string str;                                       // "20000000000000000000"
+							       // "+1"
+	std::cout << "Enter the string: " << std::endl;        // "000 +4"
+	getline(std::cin, str);                                // "wfaw 46436"
+
+	std::cout << "RESULT: " << myAtoi(str) << std::endl;
 
 	return 0;
 }
