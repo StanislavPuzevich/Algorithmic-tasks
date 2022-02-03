7dc8f0f64d4a7bc38511282973a4bcac312ae4e2
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-02-03 12:25:23 +0300

    The 11 task "Container With Most Water" LeetCode

---
+++ Algorithmic-tasks/2_MediumProblem_7.cpp
@@ -0,0 +1,47 @@
+﻿//11. Container With Most Water
+//You are given an integer array height of length n.There are n vertical lines drawn such that the two endpoints of the ith line are(i, 0)
+//and (i, height[i]).
+//Find two lines that together with the x - axis form a container, such that the container contains the most water.
+//Return the maximum amount of water a container can store.
+//Notice that you may not slant the container.
+//
+//Constraints:
+//n == height.length
+//2 <= n <= 105
+//0 <= height[i] <= 104
+//
+//Example 1 :
+//	Input : height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+//	Output : 49
+//	Explanation : The above vertical lines are represented by array[1, 8, 6, 2, 5, 4, 8, 3, 7].
+//	In this case, the max area of water(blue section) the container can contain is 49.
+
+#include <iostream>               
+#include <vector>                       
+
+
+int maxArea(std::vector<int>& height)
+{
+	int max = 0;
+	int i = 0, j = height.size() - 1;
+	while (i < j)
+	{
+		int left = height[i], right = height[j];
+		int temp = std::min(left, right) * (j - i);
+		if (temp > max)
+			max = temp;
+		if (left < right) i++;
+		else j--;
+	}
+	return max;
+}
+
+
+int main()                                                 
+{             
+	std::vector<int>vec{ 1,8,6,2,5,4,8,3,7 };
+													
+	std::cout << "RESULT: " << maxArea(vec) << std::endl;
+
+	return 0;
+}
