be10e368671da3800daafd45897c5c4e26590289
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-02-04 18:33:14 +0300

    Add files via upload

---
+++ Other Algorithms/factorial().cpp
@@ -0,0 +1,17 @@
+﻿// Факториал
+
+#include <iostream>               
+
+int factorial(int n)
+{
+	int res = n;
+	for (int i = 1; i < n; i++)
+		res *= i;
+	return res;
+}
+
+int main()                                                 
+{           
+	std::cout << "RESULT: " << factorial(5)<< std::endl;
+	return 0;
+}
