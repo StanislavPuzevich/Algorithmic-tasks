b7bbbed2423ff728f34b7be63962076853a4df0f
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-02-04 19:24:00 +0300

    Add files via upload

---
+++ Other Algorithms/pow().cpp
@@ -0,0 +1,19 @@
+﻿// Pow(x, n)
+
+#include <iostream>               
+
+
+double myPow(double x, int n)
+{
+	if (n > 0)
+		return n % 2 == 0 ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+	else if (n < 0)
+		return n % 2 == 0 ? 1 / myPow(x * x, abs(n / 2)) : 1 / (x * myPow(x * x, abs(n / 2)));
+	return 1;
+}
+
+int main()                                                 
+{
+	std::cout << "RESULT: " << myPow(3, -5) << std::endl;
+	return 0;
+}
