0c8dd52bd6dce0c88692a2da9d9e7609a6a2a443
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-02-04 19:22:28 +0300

    The 50th task "Pow(x, n)" of LeetCode

---
+++ Algorithmic-tasks/2_MediumProblem_9.cpp
@@ -0,0 +1,35 @@
+﻿//50. Pow(x, n)
+//Implement pow(x, n), which calculates x raised to the power n(i.e., xn).
+//
+//Example 2:
+//	Input: x = 2.10000, n = 3
+//	Output : 9.26100
+//
+//Example 3 :
+//	Input : x = 2.00000, n = -2
+//	Output : 0.25000
+//	Explanation : 2 - 2 = 1 / 22 = 1 / 4 = 0.25
+//
+//Constraints :
+//	-100.0 < x < 100.0
+//	- 231 <= n <= 231 - 1
+//	- 104 <= xn <= 104
+
+#include <iostream>               
+
+
+double myPow(double x, int n)
+{
+	if (n > 0)
+		return n % 2 == 0 ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+	else if (n < 0)
+		return n % 2 == 0 ? 1 / myPow(x * x, abs(n / 2)) : 1 / (x * myPow(x * x, abs(n / 2)));
+	return 1;
+}
+
+
+int main()                                                 
+{
+	std::cout << "RESULT: " << myPow(3, -5) << std::endl;
+	return 0;
+}
