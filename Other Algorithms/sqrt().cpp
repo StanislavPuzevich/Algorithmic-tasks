bd543e7b52d23edeea0ec3b554855a99a68e8845
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-02-04 18:22:39 +0300

    Add files via upload

---
+++ Other Algorithms/sqrt().cpp
@@ -0,0 +1,32 @@
+﻿//
+
+#include <iostream>               
+
+int sqrt(int n)
+{
+	int right = n, left = 0;
+	while (left <= right)
+	{
+		int mid = left + (right - left) / 2;
+
+		if (n > mid * mid)
+			left = mid + 1;
+		else if (n < mid * mid)
+			right = mid - 1;
+		else
+		{
+			right = mid;
+			break;
+		}
+	}
+	return right;
+}
+
+
+int main()                                                 
+{           
+													
+	std::cout << "RESULT: " << sqrt(1342) << std::endl;
+
+	return 0;
+}
