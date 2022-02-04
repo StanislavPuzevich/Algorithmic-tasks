d6f68ce808c29c5b16a35c467d10310d01eae918
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-02-04 18:36:09 +0300

    Add files via upload

---
+++ Other Algorithms/factorial_rec().cpp
@@ -0,0 +1,16 @@
+﻿// Факториал
+
+#include <iostream>               
+
+int factorial_rec(int n)
+{
+	if (n <= 1)
+		return 1;
+	return n * factorial_rec(n - 1);
+}
+
+int main()                                                 
+{           
+	std::cout << "RESULT: " << factorial_rec(11)<< std::endl;
+	return 0;
+}
