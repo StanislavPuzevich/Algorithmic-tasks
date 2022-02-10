d5e714ce9acce6a4712c134dc667de7ae44e0c48
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-02-10 16:11:26 +0300

    The 560 task "Subarray Sum Equals K" from LeetCode

---
+++ Algorithmic-tasks/2_MediumProblem_10.cpp
@@ -0,0 +1,42 @@
+﻿//560. Subarray Sum Equals K
+//
+//Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k
+//
+//Example 2:
+//Input: nums = [1, 2, 3], k = 3
+//Output : 2
+//
+//Constraints :
+//
+//    1 <= nums.length <= 2 * 104
+//    - 1000 <= nums[i] <= 1000
+//    - 107 <= k <= 107
+
+#include <iostream>   
+#include <vector> 
+#include <unordered_map>
+
+
+int subarraySum(std::vector<int>& nums, int k)
+{
+    int sum = 0, count = 0;
+    std::unordered_map<int, int> map;
+    for (auto i : nums)
+    {
+        sum += i;
+        if (sum == k)count++;
+        if (map.find(sum - k) != map.end()) count += map[sum - k];
+        map[sum + i]++;
+    }
+    return count;
+}
+
+
+int main()                                                 
+{
+    std::vector<int>vec{ 28,54,7,-70,22,65,-6};
+
+    std::cout << subarraySum(vec, 100) << std::endl;
+
+	return 0;
+}
