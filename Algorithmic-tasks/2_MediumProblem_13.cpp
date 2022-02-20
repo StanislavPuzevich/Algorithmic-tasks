ba4c19f26c8258a83755d8f177c49c45b9a0dc60
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-02-20 16:35:33 +0300

    2_MediumProblem_13.cpp

--- Algorithmic-tasks/2_MediumProblem_13.cpp
+++ Algorithmic-tasks/2_MediumProblem_13.cpp
@@ -1,69 +1,58 @@
-﻿//1675. Minimize Deviation in Array
+//1288. Remove Covered Intervals
 //
-//You are given an array nums of n positive integers.
-//You can perform two types of operations on any element of the array any number of times :
-//
-//If the element is even, divide it by 2.
-//For example, if the array is[1, 2, 3, 4], then you can do this operation on the last element, and the array will be[1, 2, 3, 2].
-//If the element is odd, multiply it by 2.
-//For example, if the array is[1, 2, 3, 4], then you can do this operation on the first element, and the array will be[2, 2, 3, 4].
-//The deviation of the array is the maximum difference between any two elements in the array.
-//
-//Return the minimum deviation the array can have after performing some number of operations.
+//Given an array intervals where intervals[i] = [li, ri] represent the interval[li, ri), remove all intervals that are covered by another 
+//interval in the list.
+//The interval[a, b) is covered by the interval[c, d) ifand only if c <= a and b <= d.
+//Return the number of remaining intervals.
 //
 //Example 1:
-//Input: nums = [1, 2, 3, 4]
-//Output : 1
-//Explanation : You can transform the array to[1, 2, 3, 2], then to[2, 2, 3, 2], then the deviation will be 3 - 2 = 1.
-
-//Example 2:
+//Input: intervals = [[1, 4], [3, 6], [2, 8]]
+//Output : 2
+//Explanation : Interval[3, 6] is covered by[2, 8], therefore it is removed.
 //
-//Input: nums = [4, 1, 5, 20, 3]
-//Output : 3
-//Explanation : You can transform the array after two operations to[4, 2, 5, 5, 3], then the deviation will be 5 - 2 = 3.
+//Constraints :
+//    1 <= intervals.length <= 1000
+//    intervals[i].length == 2
+//    0 <= li <= ri <= 105
+//    All the given intervals are unique.
 
 #include <iostream>   
 #include <vector>
 
 
-class Solution 
+int removeCoveredIntervals(std::vector<std::vector<int>>& intervals)
 {
-public:
-    int removeCoveredIntervals(std::vector<std::vector<int>>& intervals) 
+    bool check;
+    std::vector<std::vector<int>> res;
+
+    for (int i = 0; i < intervals.size(); i++)
     {
-        bool check;
-        std::vector<std::vector<int>> res;
-
-        for (int i = 0; i < intervals.size(); i++)
+        check = false;
+        for (int j = intervals.size() - 1; j >= 0; j--)
         {
-            check = false;
-            for (int j = intervals.size() - 1; j > 0; j--)
+            if (j != i && intervals[j][0] <= intervals[i][0] && intervals[i][1] <= intervals[j][1])
             {
-                if (j != i && intervals[j][0] <= intervals[i][0] && intervals[i][1] <= intervals[j][1])
-                {
-                    check = true;
-                    break;
-                }
+                check = true;
+                break;
             }
-            if (!check) res.push_back(intervals[i]);
         }
-
-        return res.size();
+        if (!check) res.push_back(intervals[i]);
     }
-};
+    return res.size();
+}
 
 
 int main()
 {
-    Solution s;
     std::vector<std::vector<int>> vec
     {
-        { 1, 2 },
         { 1, 4 },
+        { 2, 4 },
+        { 1, 2 },
         { 3, 4 },
     };
 
-    std::cout << s.removeCoveredIntervals(vec) << std::endl;
+    std::cout << removeCoveredIntervals(vec) << std::endl;
 
     return 0;
 }
