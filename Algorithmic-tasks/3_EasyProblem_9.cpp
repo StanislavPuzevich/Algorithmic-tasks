234898d1d1d5c4fbc77e6d0186546044e24d0621
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-02-21 20:02:13 +0300

    The 217th task "Contains Duplicate" of LeetCode

---
+++ Algorithmic-tasks/3_EasyProblem_9.cpp
@@ -0,0 +1,60 @@
+﻿//217. Contains Duplicate
+//
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+//
+//Example 1:
+//Input: nums = [1, 2, 3, 1]
+//Output : true
+//
+//Example 2 :
+//Input : nums = [1, 2, 3, 4]
+//Output : false
+//
+//Constraints :
+//    1 <= nums.length <= 105
+//    - 109 <= nums[i] <= 109
+
+#include <iostream>   
+#include <vector>
+#include <set>
+#include <algorithm>
+
+
+class Solution                        
+{
+public:
+    bool containsDuplicate1(std::vector<int>& nums)     //fast solution ~111ms
+    {
+        if (nums.size() == 1) return false;
+
+        sort(nums.begin(), nums.end());
+
+        for (int i = 1; i < nums.size(); i++)
+            if (nums[i] == nums[i - 1])
+                return true;
+
+        return false;
+    }
+
+    bool containsDuplicate2(std::vector<int>& nums)     //slow solution ~160ms
+    {
+        std::set<int> st;
+
+        for (int i = 0; i < nums.size(); i++)
+            st.insert(nums[i]);
+
+        return nums.size() == st.size() ? false : true;
+    }
+};
+
+
+int main()
+{
+    Solution s;
+    std::vector<int> vec{ 1,1,1,3,3,4,3,2,4,2 };
+
+    std::cout << s.containsDuplicate1(vec) << std::endl;
+    std::cout << s.containsDuplicate2(vec) << std::endl;
+
+    return 0;
+}
