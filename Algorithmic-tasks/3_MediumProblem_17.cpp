cb9ce2099bea03b2dd16f153ec44aefdb0775082
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-03-09 23:41:36 +0300

    82 task "Duplicates from Sorted List II" LeetCode

---
+++ Algorithmic-tasks/3_MediumProblem_17.cpp
@@ -0,0 +1,49 @@
+﻿//82. Remove Duplicates from Sorted List II
+//
+//Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.Return the linked list sorted as well.
+//
+//Example 1:
+//Input: head = [1, 2, 3, 3, 4, 4, 5]
+//Output : [1, 2, 5]
+//
+//Constraints :
+//    The number of nodes in the list is in the range[0, 300].
+//    - 100 <= Node.val <= 100
+//    The list is guaranteed to be sorted in ascending order.
+
+#include <iostream>
+
+struct ListNode 
+{
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+
+class Solution
+{
+public:
+    ListNode* deleteDuplicates(ListNode* head) 
+    {
+        ListNode* p = new ListNode(0, head);
+        ListNode* q = p;
+        bool rep = false;
+
+        while (head != nullptr)
+        {
+            if (head->next && head->val == head->next->val)
+            {
+                while (head->next && head->val == head->next->val)
+                    head = head->next;
+                q->next = head->next;
+            }
+            else
+                q = head;
+            head = head->next;
+        }
+        return p->next;
+    }
+};
