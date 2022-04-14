1b8856c4b9c82be431ad2db9c8ff4eccb5c9a133
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-04-14 09:32:13 +0300

    Searching for prime numbers in the interval

---
+++ Other Algorithms/Sieve_Eratosthenes.cpp
@@ -0,0 +1,20 @@
+//Sieve of Eratosthenes
+//search for prime numbers in the interval
+
+#include <iostream>
+
+
+int main()
+{
+	int const SIZE = 1000;
+	int mas[SIZE] = { 0 };
+
+	for (int i = 2; i * i < SIZE; i++)
+		if (mas[i] == 0)
+			for (int k = i * i; k < SIZE; k += i)
+				mas[k] = 1;
+
+	for (int i = 0; i < SIZE; i++)
+		if (mas[i] == 0)
+			std::cout << i << " ";
+}
