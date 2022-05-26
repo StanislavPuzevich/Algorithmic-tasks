3e091fca346526e890563b10314acf532883b6f0
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-05-26 10:14:13 +0300

    The 191st task "Number of 1 Bits" from LeetCode

---
+++ Algorithmic-tasks/3_EasyProblem_20.cpp
@@ -0,0 +1,58 @@
+//191. Number of 1 Bits
+//
+//------------------------------------------------
+//https://leetcode.com/problems/number-of-1-bits/
+//------------------------------------------------
+// 
+//Write a function that takes an unsigned integer and returns the number of '1' bits it has(also known as the Hamming weight).
+//
+//Example 1:
+//    Input: n = 00000000000000000000000000001011
+//    Output : 3
+//    Explanation : The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+//
+//Example 2 :
+//    Input : n = 00000000000000000000000010000000
+//    Output : 1
+//    Explanation : The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+//
+//Example 3 :
+//    Input : n = 11111111111111111111111111111101
+//    Output : 31
+//    Explanation : The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+//
+//
+//Constraints :
+//    The input must be a binary string of length 32.
+
+#include <iostream>
+
+
+class Solution 
+{
+public:
+    int hammingWeight(uint32_t n) 
+    {
+        int count = 0, x = 1;
+
+        while (n > 0)
+        {
+            long long temp = n; 
+            temp >>= x;                        // bitwise shift ( "<<" left / ">>" right)
+            if (n != (temp <<= x)) count++;
+            n = temp;
+            x++;
+        }
+        return count;
+    }
+};
+
+
+int main()
+{
+    Solution s;
+   
+    std::cout << "Result: " << s.hammingWeight(00000000000000000000000000001011) << std::endl;
+
+    return 0;
+}
