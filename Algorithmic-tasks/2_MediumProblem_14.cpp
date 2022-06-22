627565ee42ab96b636798ee146eeed08626c88c7
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-06-22 18:17:49 +0300

    The 215 task "Kth Element in an Array" LeetCode

--- Algorithmic-tasks/2_MediumProblem_14.cpp
+++ Algorithmic-tasks/2_MediumProblem_14.cpp
@@ -1,32 +1,51 @@
-﻿//215. Kth Largest Element in an Array
+//215. Kth Largest Element in an Array
+//
+//---------------------------------------------------------------
+//https://leetcode.com/problems/kth-largest-element-in-an-array/
+//---------------------------------------------------------------
 //
 //Given an integer array nums and an integer k, return the kth largest element in the array.
 //Note that it is the kth largest element in the sorted order, not the kth distinct element.
 //
 //Example 1:
-//Input: nums = [3, 2, 1, 5, 6, 4], k = 2
-//Output : 5
+//    Input: nums = [3, 2, 1, 5, 6, 4], k = 2
+//    Output : 5
 //
 //Example 2 :
-//Input : nums = [3, 2, 3, 1, 2, 4, 5, 5, 6], k = 4
-//Output : 4
+//    Input : nums = [3, 2, 3, 1, 2, 4, 5, 5, 6], k = 4
+//    Output : 4
 //
 //Constraints :
-//    1 <= k <= nums.length <= 104
-//    - 104 <= nums[i] <= 104
+//    1 <= k <= nums.length <= 10^4
+//    - 10^4 <= nums[i] <= 10^4
 
 #include <iostream>   
 #include <vector>
-#include <algorithm>
-
+#include <queue>
 
 class Solution
 {
 public:
-    int findKthLargest(std::vector<int>& nums, int k)
+    int findKthLargest_1(std::vector<int>& nums, int k)     // 1-st version (02/21/2022 20:10) using sorting;
     {
         std::sort(nums.begin(), nums.end());
         return nums[nums.size() - k];
+    }
+   
+    int findKthLargest_2(std::vector<int>& nums, int k)     // 2-nd version(06/22/18:15) using Heap(priority_queue)
+    {
+        std::priority_queue<int> que;
+
+        for (int i : nums)
+            que.push(i);
+
+        while (k > 1)
+        {
+            que.pop();
+            k--;
+        }
+
+        return que.top();
     }
 };
 
@@ -35,9 +54,7 @@ int main()
 {
     Solution s;
     std::vector<int> vec{ 3,2,3,1,2,4,5,5,6 };
-    int k = 4;
 
-    std::cout << s.findKthLargest(vec, k) << std::endl;
-
+    std::cout << s.findKthLargest(vec, 4) << std::endl;
     return 0;
 }
