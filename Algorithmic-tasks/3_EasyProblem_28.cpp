a280ba186c27fe0e26f2d3754858a514dfa9e596
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-08-24 07:14:32 +0300

    The 326th task "Power of Three" from LeetCode

---
+++ Algorithmic-tasks/3_EasyProblem_28.cpp
@@ -0,0 +1,52 @@
+﻿//326. Power of Three
+//
+//----------------------------------------------
+//https://leetcode.com/problems/power-of-three/
+//----------------------------------------------
+// 
+//Given an integer n, return true if it is a power of three.Otherwise, return false.
+//An integer n is a power of three, if there exists an integer x such that n == 3^x.
+//
+//Example 1:
+//    Input: n = 27
+//    Output : true
+//
+//Example 2 :
+//    Input : n = 0
+//    Output : false
+//
+//Example 3 :
+//    Input : n = 9
+//    Output : true
+//
+//Constraints :
+//    -2^31 <= n <= 2^31 - 1
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+class Solution
+{
+public:
+    bool isPowerOfThree(int n)
+    {
+        while (n % 3 == 0 && n > 3)
+            n /= 3;
+
+        return n == 3 || n == 1;
+    }
+};
+
+int main()
+{
+    Solution s;
+    int n;
+
+    std::cout << "Enter the n: ";
+    std::cin >> n;
+
+    std::cout << "Result: " << s.isPowerOfThree(n) << std::endl;
+
+    return 0;
+}
