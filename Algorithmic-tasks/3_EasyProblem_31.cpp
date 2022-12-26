08db3ad6cdb81eb0b6ac24f487e6c34b2c8aac6f
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-12-26 13:17:43 +0100

    The 258th task "Add Digits"

---
+++ Algorithmic-tasks/3_EasyProblem_31.cpp
@@ -0,0 +1,70 @@
+﻿//258. Add Digits
+
+//-------------------------------------------------------
+// https://leetcode.com/problems/add-digits/description/
+//-------------------------------------------------------
+// 
+//Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+//
+//Example 1:
+//	Input: num = 38
+//	Output : 2
+//	Explanation : The process is
+//	38 -- > 3 + 8 -- > 11
+//	11 -- > 1 + 1 -- > 2
+//	Since 2 has only one digit, return it.
+// 
+//Example 2:
+//	Input: num = 0
+//	Output : 0
+//
+//Constraints :
+//	0 <= num <= 231 - 1
+
+#include <iostream>
+
+class Solution 
+{
+public:
+	int addDigits(int num)			//iteration			
+	{
+		int sum = 0;
+
+		while (true)
+		{
+			while (num)
+			{
+				sum += num % 10;
+				num /= 10;
+			}
+
+			if (sum < 10) break;
+
+			num = sum;
+			sum = 0;
+		}
+
+		return sum;
+	}
+
+	int addDigits_2(int num)		//recursion
+	{
+		int sum = 0;
+		while (num)
+		{
+			sum += num % 10;
+			num /= 10;
+		}
+		if (sum < 10)
+			return sum;
+		else
+			return addDigits(sum);
+	}
+};
+
+int main()
+{
+	Solution s;
+	std::cout << "Res: " << s.addDigits(38) << std::endl;
+	return 0;
+}
