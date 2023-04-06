05967429efb96e78605d3a556ec8a5f3d7bcf25b
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2023-04-06 12:48:43 +0200

    Update 3_EasyProblem_9.cpp
    Updated the solutions.

--- Algorithmic-tasks/3_EasyProblem_9.cpp
+++ Algorithmic-tasks/3_EasyProblem_9.cpp
@@ -1,60 +1,73 @@
-﻿//217. Contains Duplicate
-//
+//217. Contains Duplicate
+// 
+//---------------------------------------------------------------
+// https://leetcode.com/problems/contains-duplicate/description/
+//---------------------------------------------------------------
+// 
 //Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 //
 //Example 1:
-//Input: nums = [1, 2, 3, 1]
-//Output : true
+//    Input: nums = [1, 2, 3, 1]
+//    Output : true
 //
 //Example 2 :
-//Input : nums = [1, 2, 3, 4]
-//Output : false
+//    Input : nums = [1, 2, 3, 4]
+//    Output : false
+//
+//Example 3 :
+//    Input : nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+//    Output : true
 //
 //Constraints :
 //    1 <= nums.length <= 105
 //    - 109 <= nums[i] <= 109
 
-#include <iostream>   
+#include <iostream>
 #include <vector>
-#include <set>
+#include <unordered_set>
 #include <algorithm>
 
-
-class Solution                        
+class Solution 
 {
 public:
-    bool containsDuplicate1(std::vector<int>& nums)     //fast solution ~111ms
+    bool containsDuplicate_vers1(std::vector<int>& nums)    // O(N*log(N))  Runtime: ~123ms Beats 91.26% | Memory: 57.4MB Beats 71.4%
+    {                                                      
+        std::sort(nums.begin(), nums.end());               
+                                                           
+        for (int i = 0, j = i + 1; j < nums.size(); i++)
+            if (nums[i] == nums[j])                        
+                return true;                               
+                                                           
+        return false;                                      
+    }                                                      
+                                                           
+    bool containsDuplicate_vers2(std::vector<int>& nums)    // O(N)         Runtime: ~160ms Beats 37.50% | Memory: 71.5MB Beats 25.41%
     {
-        if (nums.size() == 1) return false;
+        std::unordered_set<int> ust;        //std::unordered_map<int,int> ump;
 
-        sort(nums.begin(), nums.end());
+        for (auto i : nums) ust.insert(i);  //unp[i];
 
-        for (int i = 1; i < nums.size(); i++)
-            if (nums[i] == nums[i - 1])
+        return nums.size() > ust.size() ? true : false;
+    }
+
+    bool containsDuplicate_vers3(std::vector<int>& nums)    // O(N)         Runtime: ~135ms Beats 78.35% | Memory: 69.6MB Beats 52.7%
+    {
+        std::unordered_set<int> ust;
+
+        for (auto i : nums)
+            if (ust.find(i) != ust.end())
                 return true;
 
-        return false;
-    }
-
-    bool containsDuplicate2(std::vector<int>& nums)     //slow solution ~160ms
-    {
-        std::set<int> st;
-
-        for (int i = 0; i < nums.size(); i++)
-            st.insert(nums[i]);
-
-        return nums.size() == st.size() ? false : true;
+        return nums.size() > ust.size() ? true : false;
     }
 };
-
 
 int main()
 {
     Solution s;
-    std::vector<int> vec{ 1,1,1,3,3,4,3,2,4,2 };
+    std::vector<int> vec{ 5,5,3,4,1 };
 
-    std::cout << s.containsDuplicate1(vec) << std::endl;
-    std::cout << s.containsDuplicate2(vec) << std::endl;
+    std::cout << "Answer: " << s.containsDuplicate_vers2(vec) << std::endl;
 
     return 0;
 }
