1b84dd749922b0f1e4c6c31f29be02a989b88afc
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2023-05-01 11:35:40 +0200

    The 9th task "Palindrome Number" from LeetCode

--- Algorithmic-tasks/3_EasyProblem_37.cpp
+++ Algorithmic-tasks/3_EasyProblem_37.cpp
@@ -7,9 +7,9 @@
 //Given an integer x, return true if x is a palindrome, and false otherwise.
 //
 //Example 1:
-//Input: x = 121
-//Output : true
-//Explanation : 121 reads as 121 from left to right and from right to left.
+//    Input: x = 121
+//    Output : true
+//    Explanation : 121 reads as 121 from left to right and from right to left.
 //
 //Example 2 :
 //    Input : x = -121
@@ -22,7 +22,7 @@
 //    Explanation : Reads 01 from right to left.Therefore it is not a palindrome.
 //
 //Constraints :
-//    -231 <= x <= 231 - 1
+//    -2^31 <= x <= 2^31 - 1
 //
 //Follow up : Could you solve it without converting the integer to a string ?
 
