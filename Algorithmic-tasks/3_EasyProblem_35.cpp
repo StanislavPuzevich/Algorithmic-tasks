01aafd28da14f0190c07a11e538b32ed3d4486b5
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2023-05-07 17:18:45 +0200

    The 145th task "Binary Tree Postorder Traversal" from LeetCode

--- Algorithmic-tasks/3_EasyProblem_35.cpp
+++ Algorithmic-tasks/3_EasyProblem_35.cpp
@@ -1,4 +1,4 @@
-//145. Binary Tree Postorder Traversal
+//145. Binary Tree Postorder Traversal(Easy)
 //
 //---------------------------------------------------------------------------- 
 // https://leetcode.com/problems/binary-tree-postorder-traversal/description/
@@ -7,9 +7,8 @@
 //Given the root of a binary tree, return the postorder traversal of its nodes' values.
 //
 //Example 1:
-//
-//Input: root = [1, null, 2, 3]
-//Output : [3, 2, 1]
+//    Input: root = [1, null, 2, 3]
+//    Output : [3, 2, 1]
 //
 //Example 2 :
 //    Input : root = []
