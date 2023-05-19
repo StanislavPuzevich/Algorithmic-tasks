91295d1e317479e575ba4cdf4e2b2b18d170f8c0
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2023-05-19 15:51:46 +0200

    The 23d task "Merge k Sorted Lists" from LeetCode

--- Algorithmic-tasks/1_HardProblem_6.cpp
+++ Algorithmic-tasks/1_HardProblem_6.cpp
@@ -16,8 +16,8 @@
 //		1->3->4,
 //		2->6
 //	]
-//merging them into one sorted list :
-//1->1->2->3->4->4->5->6
+//	merging them into one sorted list :
+//	1->1->2->3->4->4->5->6
 //
 //Example 2 :
 //	Input : lists = []
