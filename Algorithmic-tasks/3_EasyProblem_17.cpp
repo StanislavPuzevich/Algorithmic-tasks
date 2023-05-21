4738f135fd1b307550ae7634c55c08b6fcfbecbc
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2023-05-21 13:53:53 +0200

    The 160th task "Intersection of Two Linked Lists" from LeetCode

--- Algorithmic-tasks/3_EasyProblem_17.cpp
+++ Algorithmic-tasks/3_EasyProblem_17.cpp
@@ -17,18 +17,18 @@
 //    then your solution will be accepted.
 //
 //Example 1:
-//Input: intersectVal = 8, listA = [4, 1, 8, 4, 5], listB = [5, 6, 1, 8, 4, 5], skipA = 2, skipB = 3
-//Output : Intersected at '8'
-//Explanation : The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
-//From the head of A, it reads as[4, 1, 8, 4, 5].From the head of B, it reads as[5, 6, 1, 8, 4, 5].There are 2 nodes before the intersected node in A; 
-//There are 3 nodes before the intersected node in B.
+//  Input: intersectVal = 8, listA = [4, 1, 8, 4, 5], listB = [5, 6, 1, 8, 4, 5], skipA = 2, skipB = 3
+//  Output : Intersected at '8'
+//  Explanation : The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+//  From the head of A, it reads as[4, 1, 8, 4, 5].From the head of B, it reads as[5, 6, 1, 8, 4, 5].There are 2 nodes before the intersected node in A; 
+//  There are 3 nodes before the intersected node in B.
 //
 //Constraints:
-//The number of nodes of listA is in the m.
-//The number of nodes of listB is in the n.
-//1 <= m, n <= 3 * 104
-//1 <= Node.val <= 105
-//0 <= skipA < m
+//  The number of nodes of listA is in the m.
+//  The number of nodes of listB is in the n.
+//  1 <= m, n <= 3 * 10^4
+//  1 <= Node.val <= 10^5
+//  0 <= skipA < m
 //    0 <= skipB < n
 //    intersectVal is 0 if listAand listB do not intersect.
 //    intersectVal == listA[skipA] == listB[skipB] if listAand listB intersect
