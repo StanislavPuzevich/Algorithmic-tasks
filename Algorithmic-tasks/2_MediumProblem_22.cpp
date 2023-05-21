d3f25cf66c7a7db06d36e1c4cc8dc09c0a5b3ed6
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2023-05-21 13:56:44 +0200

    The 167 task "Two SumII Input Arrs Sort" LeetCode

--- Algorithmic-tasks/2_MediumProblem_22.cpp
+++ Algorithmic-tasks/2_MediumProblem_22.cpp
@@ -7,9 +7,9 @@
 //Your solution must use only constant extra space.
 //
 //Example 1:
-//Input: numbers = [2, 7, 11, 15], target = 9
-//Output : [1, 2]
-//Explanation : The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return[1, 2].
+//    Input: numbers = [2, 7, 11, 15], target = 9
+//    Output : [1, 2]
+//    Explanation : The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return[1, 2].
 //
 //Example 2 :
 //    Input : numbers = [2, 3, 4], target = 6
@@ -17,7 +17,7 @@
 //    Explanation : The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return[1, 3].
 //
 //Constraints :
-//    2 <= numbers.length <= 3 * 104
+//    2 <= numbers.length <= 3 * 10^4
 //    - 1000 <= numbers[i] <= 1000
 //    numbers is sorted in non - decreasing order.
 //    - 1000 <= target <= 1000
