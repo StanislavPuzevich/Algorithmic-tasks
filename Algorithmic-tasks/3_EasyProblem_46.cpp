acd92ee8b63c18eebf2233ef0c6f03e38516fcc8
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2023-06-08 18:22:57 +0200

    The 1351st task "Count Negative Numbers in a Sorted Matrix" from LeetCode

--- Algorithmic-tasks/3_EasyProblem_46.cpp
+++ Algorithmic-tasks/3_EasyProblem_46.cpp
@@ -1,5 +1,9 @@
 //1351. Count Negative Numbers in a Sorted Matrix(Easy)
 //
+//--------------------------------------------------------------------------------------
+// https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/description/
+//--------------------------------------------------------------------------------------
+//
 //Given a m x n matrix grid which is sorted in non - increasing order both row - wise and column - wise, return the number of negative numbers in grid.
 //
 //Example 1:
