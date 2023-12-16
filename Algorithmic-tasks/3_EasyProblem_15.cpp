2c063416815d9d8f857a70ab130fb79d905c9f7f
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2023-12-16 18:28:45 +0100

    The 242nd task "Valid Anagram" of LeetCode

--- Algorithmic-tasks/3_EasyProblem_15.cpp
+++ Algorithmic-tasks/3_EasyProblem_15.cpp
@@ -1,34 +1,56 @@
-﻿//242. Valid Anagram
+//242. Valid Anagram(Easy)
 //
+//----------------------------------------------
+// https://leetcode.com/problems/valid-anagram/
+//---------------------------------------------- 
+// 
 //Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 //An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 //
 //Example 1:
-//Input: s = "anagram", t = "nagaram"
-//Output : true
+//    Input: s = "anagram", t = "nagaram"
+//    Output : true
 //
 //Example 2 :
-//Input : s = "rat", t = "car"
-//Output : false
+//    Input : s = "rat", t = "car"
+//    Output : false
 //
 //Constraints :
-//    1 <= s.length, t.length <= 5 * 104
+//    1 <= s.length, t.length <= 5 * 10^4
 //    s and t consist of lowercase English letters.
+//
+//Follow up : What if the inputs contain Unicode characters ? How would you adapt your solution to such a case ?
 
 #include <iostream>
-#include <vector>
 #include <algorithm>
 #include <map>
+#include <unordered_map>
 
 class Solution 
 {
 public:
+    bool isAnagram(std::string s, std::string t) // faster solution 16/12/23
+    {
+        if (s.length() != t.length())
+            return false;
+
+        std::unordered_map<char, int> ump1;
+        std::unordered_map<char, int> ump2;
+        for (int i = 0; i < s.length(); i++)
+        {
+            ump1[s[i]]++;
+            ump2[t[i]]++;
+        }
+
+        return ump1 == ump2;
+    }
+
     bool method4_isAnagram(std::string s, std::string t) // 28.07.2022 MAX short solution!
     {
         std::sort(s.begin(), s.end());
         std::sort(t.begin(), t.end());
         
-        return s == t ? true : false;
+        return s == t;
     }
     
     bool method1_isAnagram(std::string s, std::string t) // 07.03.2022 Runtime:6-8ms  USING VECTOR
